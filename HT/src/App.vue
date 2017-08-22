256515e4d1fb767e78014a168c6b637280108d23
qdez1234 <13336872253@163.com>
2017-08-22 19:46:46 +0800

    第二天

--- HT/src/App.vue
+++ HT/src/App.vue
@@ -1,81 +1,115 @@
 <template>
   <div>
-
-  <el-col id="navigation">
-    <el-menu default-active="2" class="el-menu-vertical-demo" theme="dark">
-
-     <li class="img"> 
-     <img src="./assets/logo.jpg"> 
-
-     </li>
-
-      <el-submenu index="1">
-        <template slot="title"><i class="el-icon-message"></i>导航一</template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="1-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="1-4-1">选项1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-menu-item index="2"><i class="el-icon-menu"></i>导航二</el-menu-item>
-      <el-menu-item index="3"><i class="el-icon-setting"></i>导航三</el-menu-item>
-      <el-menu-item index="3"><i class="el-icon-setting"></i>导航三</el-menu-item>
-      <el-menu-item index="3"><i class="el-icon-setting"></i>导航三</el-menu-item>
-      <el-menu-item index="3"><i class="el-icon-setting"></i>导航三</el-menu-item>
-      <el-menu-item index="3"><i class="el-icon-setting"></i>导航三</el-menu-item>
-      <el-menu-item index="3"><i class="el-icon-setting"></i>导航三</el-menu-item>
-      <el-menu-item index="3"><i class="el-icon-setting"></i>导航三</el-menu-item>
-    </el-menu>
-  </el-col>
-
-
+  	
+  	
   <div id="header">
+ <el-row>
+  <el-col :span="4"><div class="grid-content bg-purple">
+  	          <div class="Timg">
+  	          	    <img src="../dist/1.jpg" alt="" />
+  	          	    <span class="text">谢伟杰</span>
+  	          </div>
+  	
+  </div></el-col>
+  <el-col :span="11"><div class="grid-content bg-purple-light">
+  	<div class="list-text">
+  		    <span class="text">17G16A</span>
+  		    <span class="list-1">立项</span>
+  		    <span class="list-1">T1</span>
+  		    <span class="list-1">PR1</span>
+  		    <span class="list-1">PR2</span>
+  		    <span class="list-1">PR3</span>
+  		    <span class="list-1">PIR</span>
+  		    <span class="list-1">MPR</span>
+  		    <span class="list-1">...</span>
+  	</div>
+  </div></el-col>
+  <el-col :span="9" class="Hlist"><div class="grid-content bg-purple-light bg-right">
+  	        <span class="icon-1"> <i class="el-icon-date icon-1-1"></i><br /><span class="text">rom</span></span>
+  	        <span class="icon-1"> <i class="el-icon-date icon-1-1"></i><br /><span class="text">rom</span></span>
+  	        <span class="icon-1"> <i class="el-icon-date icon-1-1"></i><br /><span class="text">rom</span></span>
+  	        <span class="icon-1"> <i class="el-icon-date icon-1-1"></i><br /><span class="text">rom</span></span>
+  	        <span class="icon-1"> <i class="el-icon-date icon-1-1"></i><br /><span class="text">rom</span></span>
+  	        <span class="icon-1"> <i class="el-icon-date icon-1-1"></i><br /><span class="text">rom</span></span>
+  	        <span class="icon-1"> <i class="el-icon-date icon-1-1"></i><br /><span class="text">rom</span></span>
 
-<el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" >
-  <el-menu-item index="1" @click="goRouter(3)">处理中心</el-menu-item>
-  <el-submenu index="2" >
-    <template slot="title">我的工作台</template>
-    <el-menu-item index="2-1">选项1</el-menu-item>
-    <el-menu-item index="2-2">选项2</el-menu-item>
-    <el-menu-item index="2-3">选项3</el-menu-item>
-  </el-submenu>
-  <el-menu-item index="3" @click="goRouter(1)">表单</el-menu-item>
-  <el-menu-item index="4" @click="goRouter(2)">注册</el-menu-item>
-  <el-menu-item index="5" @click="goRouter(4)">文件上传</el-menu-item>
-  <el-menu-item index="6" @click="goRouter(5)">用户头像上传</el-menu-item>
-</el-menu>
-
+  </div></el-col>
+</el-row>
   </div>
 
+  	
+  <div id="navigation">
+  	  <el-col :span="4">
+    <el-menu default-active="2" class="el-menu-vertical-demo" theme="light">
+    <el-menu-item index="1"><i class="el-icon-menu"></i>提醒 <el-button type="success" class="number">5</el-button> <el-button type="danger" class="number">5</el-button> </el-menu-item>
+    <el-menu-item index="2"><i class="el-icon-menu"></i>特批/需批<el-button type="success" class="number">5</el-button> <el-button type="danger" class="number">5</el-button>  </el-menu-item>
+     <el-submenu index="3">
+        <template slot="title"><i class="el-icon-message"></i>通讯录</template>
+        <el-menu-item-group>
+          <el-menu-item index="3-1">誉鑫公司</el-menu-item>
+          <el-menu-item index="3-2">其他公司</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
 
+     <el-submenu index="4">
+        <template slot="title"><i class="el-icon-message"></i>部门</template>
+        <el-menu-item-group>
+          <el-menu-item index="4-1">工作日结</el-menu-item>
+          <el-menu-item index="4-2">每周日结</el-menu-item>
+          <el-menu-item index="4-3">部门经费</el-menu-item>
+          <el-menu-item index="4-4">体系文件</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
 
+     <el-submenu index="5">
+        <template slot="title"><i class="el-icon-message"></i>项目其他</template>
+        <el-menu-item-group>
+          <el-menu-item index="5-1">项目任务</el-menu-item>
+          <el-menu-item index="5-2">项目奖励</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
 
-  <div id="content">
+     <el-submenu index="6">
+        <template slot="title"><i class="el-icon-message"></i>绩效</template>
+        <el-menu-item-group>
+          <el-menu-item index="6-1">个人绩效</el-menu-item>
+          <el-menu-item index="6-2">项目绩效</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
 
-  <router-view></router-view>
+     <el-submenu index="7">
+        <template slot="title"><i class="el-icon-message"></i>知识库</template>
+        <el-menu-item-group>
+          <el-menu-item index="7-1">知识总结</el-menu-item>
+          <el-menu-item index="7-2">培训考试</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
 
+    <el-submenu index="8">
+        <template slot="title"><i class="el-icon-message"></i>设置</template>
+        <el-menu-item-group>
+          <el-menu-item index="8-1">项目团队</el-menu-item>
+        <el-submenu index="8-2">
+          <template slot="title">项目流程点</template>
+          <el-menu-item index="8-2-1">创建项目</el-menu-item>
+          <el-menu-item index="8-2-2">权限设置</el-menu-item>
+          <el-menu-item index="8-2-3">末班管理</el-menu-item>
+        </el-submenu>
+        </el-menu-item-group>
+      </el-submenu>
+       
+       <li class="Hcontent">
+      <span><i class="el-icon-star-off"></i>  <span class="timer">2017.06.06 10:05</span> <span class="text">详情详情详情情详详</span></span> 
+      <span><i class="el-icon-star-off"></i>  <span class="timer">2017.06.06 10:05</span> <span class="text">详情详情详情情详详</span></span> 
+      <span><i class="el-icon-star-off"></i>  <span class="timer">2017.06.06 10:05</span> <span class="text">详情详情详情情详详</span></span> 
+       </li>
+
+  </el-menu>
+  </el-col>
+  </div>
   </div>
 
-
-
-
-
-
-
-
-
-  </div>
 </template>
-
-
-
 
 
 <style>
@@ -83,70 +117,221 @@
       margin:0;
       padding:0;
     }
-     #header{
-        margin-left:200px;
-
-     }
-     #navigation{
-        width:200px;
-        float:left;
-     }
-     #navigation .img{
-        height:100px;
-     }
-      #navigation .img img{
-        height:60px;
-        border-radius:50%;
-        border:1px solid #ccc;
-        text-align:center;
-        margin:20px 50px;
-     }
+    html{
+    	height: 100%;
+    }
+    span{
+    	  	display: inline-block;
+    }
+    li{
+    	list-style: none;
+    }
+    body{
+    	min-width: 1190px;
+    	height: 100%;
+    }
+    body>div{
+    	min-width: 1190px;
+    	height: 100%;
+    }
       #navigation .el-menu-vertical-demo{
-          min-height:800px;
-          width:200px;
-          position:absolute;
-          top:0;
+      	  box-sizing: border-box;
+      	  background-color: white;
       }
+       #navigation .el-menu-vertical-demo>li{
+      	 padding: 0;
+      }
+       #navigation .Hcontent{
+      	  min-height:200px;
+      }
+      #navigation .Hcontent span>i{
+      	font-size: 25px;
+      }
+      #navigation .number{
+      	height: 20px;
+      	line-height: 0;
+      	margin-left: 0;
+      	margin-right: 2px;
+      	float: right;
+      	margin-top: 11px;
+      }
+      #navigation .Hcontent>span{
+      	width: 100%;
+      	box-sizing: border-box;
+      	cursor: pointer;
+      	padding-left: 10px;
+      	line-height: 10px;
+      	position: relative;
+      	margin-bottom:10px ;
+      }
+    #navigation .Hcontent>span>i{
+      	line-height: 30px;
+      	position: absolute;
+      }
+     #navigation .Hcontent>span>.timer{
+       font-size: 12px;
+       display: inline;
+       line-height: 0;
+       position: absolute;
+       top: 10px;
+       left: 50px;
+      }
+     #navigation .Hcontent>span>.text{
+       font-size: 14px;
+       top: 10px;
+       left: 40px;
+       line-height: 35px;
+       margin-top: 10px;
+       margin-left: 40px;
+       color: darkkhaki;
+       width: 160px;
+       overflow: hidden;white-space: nowrap;text-overflow: ellipsis;
+      }
+      #navigation .el-menu-vertical-demo>li{
+      	  border: 1px double  #ccc;
+      	  border-bottom: none;
+      }
+      #navigation .el-submenu .el-menu-item {
+      	min-width: auto;
+      }
+      #navigation .el-menu-vertical-demo>li:last-child{
+ 
+      }
+      #navigation .el-menu-item{
+      	height: 50px;
+      	line-height: 50px;
+      }
+       #navigation .el-menu-vertical-demo .img{
+       	height: 50px;
+       	padding-left: 20px;
+       	line-height: 50px;
+       	border: none;
+        background:white;
+        font-size: 20px;
+        font-weight:600;
+       }
+       #navigation .el-menu-vertical-demo .img img{
+       	width: 35px;
+       	height: 35px;
+       }
+       
       #content{
           margin-left:200px;
       }
+      
+</style>
+
+<style>
+  #header{
+  	border-bottom: 1px solid #ccc;
+  }
+  #header .grid-content{
+     min-height: 50px;
+     background-color: white;
+  }
+  #header .Timg{
+  	padding: 0 10px;
+  	height: 50px;
+  	line-height: 50px;
+  }
+    #header .Timg img{
+     max-height: 35px;
+     border: 1px solid #ccc;
+     border-radius: 50%;
+     margin: 5px;
+     float: left;
+  }
+     #header .Timg .text{
+       font-size: 20px;
+       font-weight: 600;
+  }
+  #header .list-text>span{
+  	height: 50px;
+  	line-height: 50px;
+  	float: left;
+  }
+    #header .list-text .text{
+    	font-size: 20px;
+    	font-weight: 800;
+    }
+   #header .list-text .list-1{
+   	border-bottom: 3px solid #ccc;
+   	width: 40px;
+   	height: 30px;
+   	text-align: center;
+   	margin-left: 15px;
+   	font-weight: 500;
+   	padding-bottom: 5px;
+   }
+   #header .list-text .list-1:last-child{
+   	border: none;
+   }
+    
+  
+  #header .bg-right {
+  	padding: 0 20px;
+  }
+  #header .bg-right>span{
+  	height: 40px;
+  	width: 50px;
+  	text-align: center;
+  }
+  #header .bg-right .icon-1{
+  	float: right;
+  }
+  #header .bg-right i{
+  	font-size: 20px;
+  	color: darkgray;
+  }
+ #header .bg-right .text{
+  	text-align: center;
+  	margin-left: 5px;
+  }
+  
+  #header .Hlist{
+  	height: 50px;
+  }
+  
+    #header .el-row{
+	  margin-bottom: 0;
+     }
+     #header .el-row .grid-content{
+     	border-radius: 0;
+     }
+	  .el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
 </style>
 
 
-<script>
 
+<script>
   export default {
-    data() {
-     return {
-        activeIndex: '1',
-        activeIndex2: '1'
-      };
-    },
-     methods: {
-         goRouter(index){
-              switch (index) {
-                      case 1:
-                        this.$router.push({path: 'ace'});
-                        console.log(this)
-                        break;
-                      case 2:
-                        this.$router.push({path: 'acf'});
-                        console.log(this)
-                        break;
-                      case 3:
-                        this.$router.push({path: 'acg'});
-                        console.log(this)
-                        break;
-                      case 4:
-                        this.$router.push({path: 'ach'});
-                        console.log(this)
-                        break;
-                      case 5:
-                        this.$router.push({path: 'aci'});
-                        console.log(this)
-                        break;
-                    }
-                }
-            },
+    methods: {
+
+    }
   }
 </script>
